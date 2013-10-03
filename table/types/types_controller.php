023541ce6215f86e7ca2242130650e90338f5eb5
Eric Hoppe <dash55@gmail.com>
2013-10-03 12:53:20 -0400

    Finished table\types

--- table/types/types_controller.php
+++ table/types/types_controller.php
@@ -5,9 +5,21 @@ function initialize()
     {
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
-        if ($_POST['new_type'] and $_POST['new_split'])
+        if (isset($_POST['new_type']) and isset($_POST['new_split']))
             {
             create($_POST['new_type'], $_POST['new_split']);
+            }
+        elseif (isset($_POST['spl_type']) and isset($_POST['spl_new_split']))
+            {
+            split_change($_POST['spl_type'], $_POST['spl_new_split']);
+            }
+        elseif (isset($_POST['type_old']) and isset($_POST['type_new']))
+            {
+            type_change($_POST['type_old'], $_POST['type_new']);
+            }
+        elseif (isset($_POST['del_type']))
+            {
+            destroy($_POST['del_type']);
             }
         }
     }
--- table/types/types_view.php
+++ table/types/types_view.php
@@ -3,12 +3,12 @@ include_once 'types_controller.php';
 
 initialize();
 
-$table = show_table();
 ?>
 
 <table border="1">
 <tr><th>Type</th><th>Split</th></tr>
 <?php
+$table = show_table();
 while ($row = mysqli_fetch_array($table))
     {
     ?>
@@ -26,4 +26,24 @@ while ($row = mysqli_fetch_array($table))
 New Type: <input type="text" name="new_type" /><br />
 Dealer Split: <input type="text" name="new_split" /><br />
 <input type="submit" name="Create New" />
+</form>
+
+<b>Change Split</b>
+<form action="" method="POST">
+Dealer Type: <input type="text" name="spl_type" /><br />
+New Split: <input type="text" name="spl_new_split" /><br />
+<input type="submit" name="Change Split" />
+</form>
+
+<b>Change Type ID</b>
+<form action="" method="POST">
+Old Type: <input type="text" name="type_old" /><br />
+New Type: <input type="text" name="type_new" /><br />
+<input type="submit" name="Change ID" />
+</form>
+
+<b>Delete Type</b>
+<form action="" method="POST">
+Type: <input type="text" name="del_type" /><br />
+<input type="submit" name="Delete Type" />
 </form>
