76e22e5d7edee4adac275ce0c92cc7366b452e8f
Eric Hoppe <dash55@gmail.com>
2013-10-09 15:11:03 -0400

    Fixed types_controller.php
    + It came to my attention in writing the dealer creation controller that
    isset returns true for empty strings. Backwards as that is, I came back
    and changed all instances of isset in types_controller.php to !empty so
    that all four forms don't run at once half filled out or anything.

--- table/types/types_controller.php
+++ table/types/types_controller.php
@@ -5,19 +5,19 @@ function initialize()
     {
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
-        if (isset($_POST['new_type']) and isset($_POST['new_split']))
+        if (!empty($_POST['new_type']) and !empty($_POST['new_split']))
             {
             create($_POST['new_type'], $_POST['new_split']);
             }
-        elseif (isset($_POST['spl_type']) and isset($_POST['spl_new_split']))
+        elseif (!empty($_POST['spl_type']) and !empty($_POST['spl_new_split']))
             {
             split_change($_POST['spl_type'], $_POST['spl_new_split']);
             }
-        elseif (isset($_POST['type_old']) and isset($_POST['type_new']))
+        elseif (!empty($_POST['type_old']) and !empty($_POST['type_new']))
             {
             type_change($_POST['type_old'], $_POST['type_new']);
             }
-        elseif (isset($_POST['del_type']))
+        elseif (!empty($_POST['del_type']))
             {
             destroy($_POST['del_type']);
             }
